da780ae8208ac7d2a4130e4db0f5c58cd6d6745b
Victor Hugo <vh.pegoraro@gmail.com>
2016-04-26 19:10:35 -0300

    Removing logger on server

--- server.js
+++ server.js
@@ -4,15 +4,6 @@ var app = express();
 var PORT = 3000;
 var FOLDER = __dirname + "/public/";
 
-var middleware = {
-	logger: function(req, res, next) {
-		console.log(req.method + ': ' + req.originalUrl);
-		next();
-	}
-};
-
-app.use(middleware.logger);
-
 app.use(express.static(FOLDER));
 
 app.get("/*", function(req, res) {
