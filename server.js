8126c5559d431fbbcf506dd8344c33c81427e3e9
Victor Hugo <vh.pegoraro@gmail.com>
2016-04-26 19:56:19 -0300

    Update server port for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.port || 3000;
 var FOLDER = __dirname + "/public/";
 
 app.use(express.static(FOLDER));
